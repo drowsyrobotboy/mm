6e1b2bf996e437f3ac81d074ac3fbbd1ea8eaf32
Maruthi Sharma <maruthisharma@live.in>
2016-09-21 15:49:32 +0530

    alternative smooth scroll

--- js/main.js
+++ js/main.js
@@ -56,18 +56,3 @@ $(window).on("hashchange", function(){
     slideout.close();
   }
 });
-
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
