1e10c6765a2e50811282ddfc878a2763ccfd994e
Maruthi Sharma <maruthisharma@live.in>
2016-09-21 15:42:55 +0530

    smooth scroll

--- js/main.js
+++ js/main.js
@@ -56,3 +56,18 @@ $(window).on("hashchange", function(){
     slideout.close();
   }
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
